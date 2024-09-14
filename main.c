258c8c8efe09608ab01136cb5143b91d5e3abc56
Corey Williamson <corey.bailey.williamson@gmail.com>
2024-09-14 09:19:38 -0700

    Working keyboard bt example with typing on serial port

--- main.c
+++ main.c
@@ -3,18 +3,34 @@
  *
  * SPDX-License-Identifier: Apache-2.0
  */
-
+// #include "FreeRTOS.h"
+// #include "task.h"
 #include "pico/cyw43_arch.h"
 #include "pico/stdlib.h"
 #include "btstack_run_loop.h"
 #include "hardware/uart.h"
+#include "tusb.h"
 
 int btstack_main(int argc, const char* argv[]);
+
+// void vApplicationStackOverflowHook(TaskHandle_t pxTask, char* pcTaskName)
+// {
+// }
+// void vApplicationTickHook(void)
+// {
+// }
+// void vApplicationMallocFailedHook(void)
+// {
+// }
 
 int main()
 {
 	stdio_init_all();
-
+	// To make execution wait for usb serial connection
+	while(!tud_cdc_connected())
+	{
+		sleep_ms(100);
+	};
 	// initialize CYW43 driver
 	if(cyw43_arch_init())
 	{
