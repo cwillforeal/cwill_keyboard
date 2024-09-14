d6b2200c1c29eb58392ab76f61717a3165b566e1
Corey Williamson <corey.bailey.williamson@gmail.com>
2024-09-14 12:03:15 -0700

    Got the bt in a freertos task and running

--- main.c
+++ main.c
@@ -3,8 +3,7 @@
  *
  * SPDX-License-Identifier: Apache-2.0
  */
-// #include "FreeRTOS.h"
-// #include "task.h"
+#include "keyboard_tasks.h"
 #include "pico/cyw43_arch.h"
 #include "pico/stdlib.h"
 #include "btstack_run_loop.h"
@@ -13,16 +12,42 @@
 
 int btstack_main(int argc, const char* argv[]);
 
-// void vApplicationStackOverflowHook(TaskHandle_t pxTask, char* pcTaskName)
-// {
-// }
-// void vApplicationTickHook(void)
-// {
-// }
-// void vApplicationMallocFailedHook(void)
-// {
-// }
+/* Stubs for FreeRTOS */
+void vApplicationStackOverflowHook(TaskHandle_t pxTask, char* pcTaskName)
+{
+}
+void vApplicationMallocFailedHook(void)
+{
+}
 
+static void task_ble(void* pv)
+{
+	if(cyw43_arch_init())
+	{
+		printf("cyw43_arch_init() failed.\n");
+		while(1)
+			;
+	}
+
+	// run the app
+	btstack_main(0, NULL);
+	while(1)
+	{
+		btstack_run_loop_execute(); /* Does not return */
+	}
+}
+
+void main_ble_setup(void)
+{
+	if(xTaskCreate(task_ble, "ble_task", 1200 / sizeof(StackType_t), (void*)NULL, BLE_TASK_PRIORITY,
+				   (TaskHandle_t*)NULL) != pdPASS)
+	{
+		printf("BLE task creat fail.\n");
+		for(;;)
+		{
+		} /* error! probably out of memory */
+	}
+}
 int main()
 {
 	stdio_init_all();
@@ -31,14 +56,6 @@ int main()
 	{
 		sleep_ms(100);
 	};
-	// initialize CYW43 driver
-	if(cyw43_arch_init())
-	{
-		printf("cyw43_arch_init() failed.\n");
-		return -1;
-	}
-
-	// run the app
-	btstack_main(0, NULL);
-	btstack_run_loop_execute();
+	main_ble_setup();
+	vTaskStartScheduler();
 }
